5c20451b65eceaa42cb9e0f3f3ed7ff486d8f3ba
Satish Kumar <satishkumar2110411@ssn.edu.in>
2021-11-26 18:55:41 +0530

    exercise 4

--- college_files/ex4.py
+++ college_files/ex4.py
@@ -60,3 +60,66 @@ def newton_root(num,iteration):
         num = 0.5*(num + x/num)
     return(num)
 
+def program():
+    print('Enter the program you want to access: \
+        \n 1. Greatest common divisor \n 2. Prime number \
+        \n 3. Integer division \n 4. Sum of digits of a number \
+        \n 5. Multiplication table \n 6. Sum of series \
+        \n 7. Square root of a number using Newton\'s method')
+    
+    status = 0
+    while status == 0:
+        problem = int(input('Enter your choice: '))
+        if problem == 1:
+            print('Greatest common divisor')
+            a = int(input('Enter the first number: '))
+            b = int(input('Enter the second number: '))
+            result = gcd(a,b)
+            print(f'GCD = {result}')
+    
+        elif problem ==2:
+            print('Prime number')
+            num = int(input('Enter number to be checked: '))
+            result = prime_number(num)
+            print(result)
+    
+        elif problem == 3:
+            pass
+
+        elif problem == 4:
+            print('Sum of digits of a number')
+            num = int(input('Enter the number: '))
+            print(f'The sum of the digits is {digit_sum(num)}')
+    
+        elif problem == 5:
+            print('Multiplication table')
+            x = int(input('Enter the table number: '))
+            multiplication_table(x)
+    
+        elif problem == 6:
+            print('Sum of series')
+            n = int(input('Enter the number of values in the series: '))
+            list_ =[]
+            for i in range(n):
+                value = float(input('Enter the number: '))
+                list_ += [value,]
+            else:
+                print('The sum of the series is: ',sum_of_series(list_))
+    
+        elif problem == 7:
+            print('Square root of a number using Newton\'s method')
+            num = int(input('Enter the number: '))
+            iteration_value = int(input('Enter the number of times newton\'s method must be applied: '))
+            result = newton_root(num, iteration_value)
+            print(f'Root of {num} = {result}')
+
+        else:
+            print('Invalid problem number')
+
+        reply = input('Do you want to continue? (y/n): ')
+        if reply == 'n':
+            status = 1
+        
+    
+if __name__ == '__main__':
+    program()
